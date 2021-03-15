8505b8a4562febacca37c7ae6dc5807b9ffcb625
Diana Sivitkaya <di.sivitskaya@gmail.com>
2021-03-16 01:35:20 +0300

    feat: implement function from task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    if (str.length % 2 !== 0) return false;
+    let arr = str.split("");
+    let newarr = bracketsConfig.flat();
+
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < newarr.length; j++) {
+            if (arr[i] === newarr[j] && arr[i + 1] === newarr[j + 1]) {
+                // console.log('было' + arr);
+
+                arr.splice(i, 2);
+                i = i - 2;
+                // console.log('стало' + arr);
+            }
+        }
+    }
+    return arr.length === 0 ? true : false;
+};
